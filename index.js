948dbe634d926e2362b620730071bc2b577c49c9
madhu <rayi.madhupriya@gmail.com>
2024-03-26 23:15:52 +0530

    Added chat logic

--- index.js
+++ index.js
@@ -2,25 +2,53 @@ const express= require('express');
 const http= require('http');
 const socketio= require('socket.io');
 
+const connect= require('./config/database-config');
+
+const Chat= require('./models/chat');
+
 const app= express();
 const server= http.createServer(app);
 const io= socketio(server);
 
  io.on('connection',(socket)=>{
-    console.log("a user connected",socket.id);
-
-    socket.on('from_client',()=>{
-        console.log("event coming from client");
+    socket.on('join_room',(data)=>{
+        socket.join(data.roomid);
     })
 
-    setInterval(()=>{
-        socket.emit("from_server");
-    },2000);
- })  
+    socket.on('msg_send',async(data)=>{
+        console.log(data);
+        const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        })
+        io.to(data.roomid).emit('msg_rcvd',data);
+        //socket.emit('msg_rcvd',data);
+        //socket.broadcoast.emit('msg_rcvd',data);
 
+    })
+    socket.on('typing',(data)=>{
+        socket.broadcast.to(data.roomid).emit('someone_typing');
+    })
+
+
+ })  
+app.set('view engine','ejs');
 app.use('/',express.static(__dirname+'/public'));
 
-server.listen(3008,()=>{
-    console.log("Server started");
+app.get('/chat/:roomid', async (req,res)=>{
+    const chats=  await Chat.find({
+        roomId: req.params.roomid 
+    }).select('content user');
+    res.render('index',{
+        name: 'Madhu',
+        id: req.params.roomid,
+        chats: chats
+    });
+});
 
+server.listen(3008,async ()=>{
+    console.log("Server started");
+    await connect();
+    console.log("mongodb connected");
 }) 
--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
-var socket= io();
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here
+    var socket = io();
+    
+    let btn = document.getElementById("btn");
+    let inputmsg = document.getElementById("newmsg");
+    let msglist = document.getElementById("msglist");
 
-let btn= document.getElementById('btn');
-btn.onclick = function exec(){
-    socket.emit('from_client');
-}
-socket.on('from_server',()=>{
-    // console.log("collected a new event from server");
-    const div= document.createElement('div');
-    div.innerText= 'New event from server';
-    // console.log(div);
-    document.body.appendChild(div);
-})
+    btn.onclick = function exec() {
+        socket.emit('msg_send', {
+            msg: inputmsg.value
+        });
+    };
+    socket.on('msg_rcvd',(data)=>{
+        let limsg= document.createElement('li');
+        limsg.innerText= data.msg;
+        msglist.appendChild(limsg);
+    })
+});
+
